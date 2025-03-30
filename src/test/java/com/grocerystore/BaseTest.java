7ad381a2dbc2a155504e8df054b3fd19e20eac5f
Guilherme Reis <33372432+guilherme-reis@users.noreply.github.com>
2025-03-30 14:34:34 -0700

    Get cart contents test passing using BaseTest setup

--- src/test/java/com/grocerystore/BaseTest.java
+++ src/test/java/com/grocerystore/BaseTest.java
@@ -1,21 +1,34 @@
 package com.grocerystore;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
 public class BaseTest {
-    protected static String baseUri = "https://simple-grocery-store-api.glitch.me";
+
+    protected static final String baseUri = "https://simple-grocery-store-api.glitch.me";
     protected static String accessToken;
 
     @BeforeClass
-    public void authenticate() {
-        accessToken = RestAssured
-            .given()
-            .baseUri(baseUri)
-            .contentType("application/json")
-            .body("{\"email\": \"user" + System.currentTimeMillis() + "@example.com\"}")
-            .post("/auth/register")
-            .jsonPath()
-            .getString("accessToken");
+    public void generateAccessToken() {
+        if (accessToken == null) {
+            String email = "user" + System.currentTimeMillis() + "@example.com";
+
+            Response response = RestAssured
+                .given()
+                .baseUri(baseUri)
+                .contentType("application/json")
+                .body("{\"clientName\": \"api-test\", \"clientEmail\": \"" + email + "\"}")
+                .post("/api-clients");
+
+            assertEquals(response.statusCode(), 201);
+            accessToken = response.jsonPath().getString("accessToken");
+            assertNotNull(accessToken);
+        }
+
+        RestAssured.baseURI = baseUri;
     }
 }
---
+++ src/test/java/com/grocerystore/GetCartContentsTest.java
@@ -0,0 +1,40 @@
+package com.grocerystore;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+public class GetCartContentsTest extends BaseTest {
+
+    private String cartId;
+
+    @BeforeClass
+    public void ensureCartExists() {
+        cartId = System.getProperty("cartId");
+        if (cartId == null) {
+            Response createResponse = RestAssured
+                .given()
+                .header("Authorization", "Bearer " + accessToken)
+                .post("/carts");
+
+            assertEquals(createResponse.statusCode(), 201);
+            cartId = createResponse.jsonPath().getString("cartId");
+            assertNotNull(cartId);
+            System.setProperty("cartId", cartId);
+        }
+    }
+
+    @Test
+    public void getCartContentsShouldReturn200() {
+        Response response = RestAssured
+            .given()
+            .header("Authorization", "Bearer " + accessToken)
+            .get("/carts/" + cartId);
+
+        assertEquals(response.statusCode(), 200);
+    }
+}

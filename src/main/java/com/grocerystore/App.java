07e699742dd53294e9222c1e05e84df15a27420c
Guilherme Reis <33372432+guilherme-reis@users.noreply.github.com>
2025-03-30 13:52:01 -0700

    Initial setup for Grocery API Tests

---
+++ src/main/java/com/grocerystore/App.java
@@ -0,0 +1,13 @@
+package com.grocerystore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

8770aac0e880e253e5d8fb6a39729f95c9aae713
Guilherme Reis <33372432+guilherme-reis@users.noreply.github.com>
2025-03-30 17:12:40 -0700

    Enhance CartCreationTest with additional assertions

--- src/test/java/com/grocerystore/CartCreationTest.java
+++ src/test/java/com/grocerystore/CartCreationTest.java
@@ -1,23 +1,26 @@
 package com.grocerystore;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
 public class CartCreationTest extends BaseTest {
+
     @Test
     public void createCartShouldReturnCartId() {
-        Response response = RestAssured
-            .given()
-            .baseUri(baseUri)
+        Response response = given()
             .header("Authorization", "Bearer " + accessToken)
             .post("/carts");
 
         assertEquals(response.statusCode(), 201);
+        boolean created = response.jsonPath().getBoolean("created");
         String cartId = response.jsonPath().getString("cartId");
+
+        assertTrue(created);
         assertNotNull(cartId);
         System.setProperty("cartId", cartId);
     }

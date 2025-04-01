5460c23210d71fb0ba52a918080a110eccdf2886
Guilherme Reis <33372432+guilherme-reis@users.noreply.github.com>
2025-03-31 20:48:57 -0700

    Update StatusTest.java

--- src/test/java/com/grocerystore/StatusTest.java
+++ src/test/java/com/grocerystore/StatusTest.java
@@ -1,17 +1,22 @@
 package com.grocerystore;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class StatusTest extends BaseTest {
+
     @Test
-    public void checkStatusReturns200() {
-        Response response = RestAssured
-            .given()
-            .get("/status");
+    public void checkStatusReturns200AndValidBody() {
+        Response response = given().get("/status");
+
         assertEquals(response.statusCode(), 200);
+
+        String status = response.jsonPath().getString("status");
+        assertNotNull(status);
+        assertEquals(status.toLowerCase(), "up");
     }
 }
